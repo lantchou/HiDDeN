95339943c541abd6e660e0f31e8bd6e9589f7606
Lucas Antchougov <lucasangov@hotmail.com>
2023-03-25 16:29:28 +0100

    Do random crop instead of left upper corner

--- test_model.py
+++ test_model.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import csv
-import math
 import numpy as np
 import torch
 import torch.nn
@@ -15,14 +14,6 @@ from noise_layers.noiser import Noiser
 
 TEST_IMAGES_FOLDER = "../data/test/"
 
-
-def random_crop(img, height, width):
-    assert img.shape[0] >= height
-    assert img.shape[1] >= width
-    x = np.random.randint(0, img.shape[1] - width)
-    y = np.random.randint(0, img.shape[0] - height)
-    img = img[y:y+height, x:x+width]
-    return img
 
 
 def main():
@@ -104,8 +95,8 @@ def load_test_images(img_size, device, resize=True, size=1000):
                 if img.size[0] < img_size or img.size[1] < img_size:
                     img.close()
                     continue
-                np_img = np.array(img)
-                np_img = np_img[:img_size, :img_size]
+
+                np_img = random_crop(img, img_size, img_size)
             img.close()
             images.append(TF.to_tensor(np_img).unsqueeze_(0))
 
@@ -114,6 +105,17 @@ def load_test_images(img_size, device, resize=True, size=1000):
     images_tensor = torch.cat(images).to(device)
     images_tensor = images_tensor * 2 - 1  # transform from [0, 1] to [-1, 1]
     return images_tensor
+
+
+def random_crop(img, width, height):
+    img_width, img_height = img.size
+    img = np.array(img)
+    assert img_width >= width
+    assert img_height >= height
+    x_start = np.random.randint(0, img_width - width)
+    y_start = np.random.randint(0, img_height - height)
+    img = img[y_start:y_start+height, x_start:x_start+width, :]
+    return img
 
 
 def save_results(images, messages, hidden_net, args, message_length, experiment_name):
