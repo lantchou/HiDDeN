99c23e05de054ebdd4a2459002fad2b8b93ab534
Lucas Antchougov <lucasangov@hotmail.com>
2023-07-11 12:01:16 +0200

    Use zigzag indeces in jpeg noise layer

--- noise_layers/jpeg_compression.py
+++ noise_layers/jpeg_compression.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import os
 import torchvision
 
+
 def gen_filters(size_x: int, size_y: int, dct_or_idct_fun: callable) -> np.ndarray:
     tile_size_x = 8
     filters = np.zeros((size_x * size_y, size_x, size_y))
@@ -12,10 +13,12 @@ def gen_filters(size_x: int, size_y: int, dct_or_idct_fun: callable) -> np.ndarr
         for k_x in range(size_x):
             for n_y in range(size_y):
                 for n_x in range(size_x):
-                    filters[k_y * tile_size_x + k_x, n_y, n_x] = dct_or_idct_fun(n_y, k_y, size_y) * dct_or_idct_fun(n_x,
-                                                                                                            k_x,
-                                                                                                            size_x)
+                    filters[k_y * tile_size_x + k_x, n_y, n_x] = dct_or_idct_fun(n_y, k_y, size_y) * dct_or_idct_fun(
+                        n_x,
+                        k_x,
+                        size_x)
     return filters
+
 
 # def zigzag_indices(shape: (int, int), count):
 #     x_range, y_range = shape
@@ -29,13 +32,53 @@ def gen_filters(size_x: int, size_y: int, dct_or_idct_fun: callable) -> np.ndarr
 #     return mask
 
 
+def zigzag_indices(n):
+    indices = []
+    i = 0
+    j = 0
+    count = 0
+
+    while count < n:
+        # Upper right triangle
+        while i >= 0:
+            indices.append((i, j))
+            count += 1
+
+            if count == n:
+                break
+
+            i -= 1
+            j += 1
+
+        if count == n:
+            break
+
+        i = 1
+        # Lower left triangle
+        while j >= 0:
+            indices.append((i, j))
+            count += 1
+
+            if count == n:
+                break
+
+            i += 1
+            j -= 1
+
+        i += 1
+        j = 1
+
+    return torch.tensor(indices)
+
+
 def get_jpeg_yuv_filter_mask(image_shape: tuple, window_size: int, keep_count: int):
     mask = np.zeros((window_size, window_size), dtype=np.uint8)
 
-    index_order = sorted(((x, y) for x in range(window_size) for y in range(window_size)),
-                         key=lambda p: (p[0] + p[1], -p[1] if (p[0] + p[1]) % 2 else p[1]))
+    # index_order = sorted(((x, y) for x in range(window_size) for y in range(window_size)),
+    #                      key=lambda p: (p[0] + p[1], -p[1] if (p[0] + p[1]) % 2 else p[1]))
+    indeces = zigzag_indices(keep_count)
 
-    for i, j in index_order[0:keep_count]:
+    for i, j in indeces:
         mask[i, j] = 1
 
     return np.tile(mask, (int(np.ceil(image_shape[0] / window_size)),
@@ -53,15 +96,25 @@ def idct_coeff(n, k, N):
 
 def rgb2yuv(image_rgb, image_yuv_out):
     """ Transform the image from rgb to yuv """
-    image_yuv_out[:, 0, :, :] = 0.299 * image_rgb[:, 0, :, :].clone() + 0.587 * image_rgb[:, 1, :, :].clone() + 0.114 * image_rgb[:, 2, :, :].clone()
-    image_yuv_out[:, 1, :, :] = -0.14713 * image_rgb[:, 0, :, :].clone() + -0.28886 * image_rgb[:, 1, :, :].clone() + 0.436 * image_rgb[:, 2, :, :].clone()
-    image_yuv_out[:, 2, :, :] = 0.615 * image_rgb[:, 0, :, :].clone() + -0.51499 * image_rgb[:, 1, :, :].clone() + -0.10001 * image_rgb[:, 2, :, :].clone()
+    image_yuv_out[:, 0, :, :] = 0.299 * image_rgb[:, 0, :, :].clone() + 0.587 * image_rgb[:, 1, :,
+                                                                                :].clone() + 0.114 * image_rgb[:, 2, :,
+                                                                                                     :].clone()
+    image_yuv_out[:, 1, :, :] = -0.14713 * image_rgb[:, 0, :, :].clone() + -0.28886 * image_rgb[:, 1, :,
+                                                                                      :].clone() + 0.436 * image_rgb[:,
+                                                                                                           2, :,
+                                                                                                           :].clone()
+    image_yuv_out[:, 2, :, :] = 0.615 * image_rgb[:, 0, :, :].clone() + -0.51499 * image_rgb[:, 1, :,
+                                                                                   :].clone() + -0.10001 * image_rgb[:,
+                                                                                                           2, :,
+                                                                                                           :].clone()
 
 
 def yuv2rgb(image_yuv, image_rgb_out):
     """ Transform the image from yuv to rgb """
     image_rgb_out[:, 0, :, :] = image_yuv[:, 0, :, :].clone() + 1.13983 * image_yuv[:, 2, :, :].clone()
-    image_rgb_out[:, 1, :, :] = image_yuv[:, 0, :, :].clone() + -0.39465 * image_yuv[:, 1, :, :].clone() + -0.58060 * image_yuv[:, 2, :, :].clone()
+    image_rgb_out[:, 1, :, :] = image_yuv[:, 0, :, :].clone() + -0.39465 * image_yuv[:, 1, :,
+                                                                           :].clone() + -0.58060 * image_yuv[:, 2, :,
+                                                                                                   :].clone()
     image_rgb_out[:, 2, :, :] = image_yuv[:, 0, :, :].clone() + 2.03211 * image_yuv[:, 1, :, :].clone()
 
 
@@ -83,7 +136,6 @@ class JpegCompression(nn.Module):
         # create a new large mask which we can use by slicing for images which are smaller
         self.create_mask((1000, 1000))
 
-
     def create_mask(self, requested_shape):
         if self.jpeg_mask is None or requested_shape > self.jpeg_mask.shape[1:]:
             self.jpeg_mask = torch.empty((3,) + requested_shape, device=self.device)
@@ -96,7 +148,6 @@ class JpegCompression(nn.Module):
             self.create_mask(image_shape)
         # return the correct slice of it
         return self.jpeg_mask[:, :image_shape[1], :image_shape[2]].clone()
-
 
     def apply_conv(self, image, filter_type: str):
 
@@ -115,8 +166,8 @@ class JpegCompression(nn.Module):
             image_conv = image_conv.view(image_conv.shape[0], image_conv.shape[1], image_conv.shape[2], 8, 8)
             image_conv = image_conv.permute(0, 1, 3, 2, 4)
             image_conv = image_conv.contiguous().view(image_conv.shape[0],
-                                                  image_conv.shape[1]*image_conv.shape[2],
-                                                  image_conv.shape[3]*image_conv.shape[4])
+                                                      image_conv.shape[1] * image_conv.shape[2],
+                                                      image_conv.shape[3] * image_conv.shape[4])
 
             image_conv.unsqueeze_(1)
 
@@ -126,7 +177,6 @@ class JpegCompression(nn.Module):
         image_conv_stacked = torch.cat(image_conv_channels, dim=1)
 
         return image_conv_stacked
-
 
     def forward(self, noised_and_cover):
 
@@ -161,10 +211,12 @@ class JpegCompression(nn.Module):
         yuv2rgb(image_idct, image_ret_padded)
 
         # un-pad
-        noised_and_cover[0] = image_ret_padded[:, :, :image_ret_padded.shape[2]-pad_height, :image_ret_padded.shape[3]-pad_width].clone()
+        noised_and_cover[0] = image_ret_padded[:, :, :image_ret_padded.shape[2] - pad_height,
+                              :image_ret_padded.shape[3] - pad_width].clone()
         # save_images(noised_and_cover[0], "output.png", ".")
 
         return noised_and_cover
+
 
 def save_images(original_images, filename, folder):
     n, _, h, w = original_images.shape
@@ -175,4 +227,3 @@ def save_images(original_images, filename, folder):
 
     path = os.path.join(folder, filename)
     torchvision.utils.save_image(images, path)
-
